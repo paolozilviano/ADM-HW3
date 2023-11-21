c31240ff69f490c13201c34275ed33c898b7d501
Paolo Zilviano <100868577+paolozilviano@users.noreply.github.com>
2023-11-21 15:57:05 +0100

    Update CommandLine.sh

--- Command Line Question/CommandLine.sh
+++ Command Line Question/CommandLine.sh
@@ -2,7 +2,7 @@ country=$(cut -f11 -d$'\t' correct_merged_courses.tsv | sort | uniq -c | sort -n
 city=$(cut -f10 -d$'\t' correct_merged_courses.tsv | sort | uniq -c | sort -nr | head -n 1 | awk '{ $1=""; print $0}' | sed -e 's/^[[:space:]]*//')
 echo "The country that offers the majority of masters in the website is $country, while the city is $city"
 
-count=$(cut -f9 correct_merged_courses.tsv | grep -i -E "part[- ]?time" | wc -l)
+count=$(cut -f9 correct_merged_courses.tsv | grep -i -E "part[- ]?time" | wc -l) ##kk
 echo "In total $count masters offer a part-time education"
 
 total=$(awk 'END {print NR-1}' correct_merged_courses.tsv)
